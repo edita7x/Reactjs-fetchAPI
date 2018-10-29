fdb8a26fa204ca29ebff7ecee0069c36452e41ad
edita7x <edita7x@gmail.com>
2018-10-29 19:44:08 +0700

    first commit

---
+++ src/Main.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {Route, NavLink, HashRouter} from "react-router-dom";
+import Home from "./Home";
+import Australia from "./Australia";
+import Turkey from "./Turkey";
+import About from "./About";
+
+class Main extends Component{
+    render(){
+        return(
+            <HashRouter>
+                <div>
+                    <h1 className="title">Fetching Data API</h1>
+                    <ul className="header">
+                        <li><NavLink exact to ="/">Home</NavLink></li>
+                        <li><NavLink to ="/australia">Australia</NavLink></li>
+                        <li><NavLink to ="/turkey">Turkey</NavLink></li>
+                        <li><NavLink to ="/about">About</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route exact path ="/" component={Home}/>
+                        <Route path="/australia" component={Australia}/>
+                        <Route path="/turkey" component={Turkey}/>
+                        <Route path="/about" component={About}/>
+                    </div>
+                </div>
+            </HashRouter>
+        )
+    }
+}
+
+export default Main;
